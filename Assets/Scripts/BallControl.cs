4d78adf5adc2f79174f8d68a2895bf8829652ad7
fajarbinus <72683008+fajarbinus@users.noreply.github.com>
2024-04-01 12:36:14 +0700

    Update 2024.4.1
    -Post Procesiing
    -Polishing
    -Bug Ball Bounce Fixed

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BallControl : MonoBehaviour
@@ -12,7 +13,6 @@ public class BallControl : MonoBehaviour
     {
         rb2d = GetComponent<Rigidbody2D>();
         Invoke("GoBall",2f);
-        
     }
 
     void GoBall()
@@ -27,10 +27,70 @@ public class BallControl : MonoBehaviour
         //Debug.Log("Restart!!!");
         ResetBall();
         Invoke("GoBall",2f);
+        wallCollisionCount = 0;
     }
     private void ResetBall()
     {
         rb2d.velocity = Vector2.zero;
         transform.position = Vector2.zero;
     }
+
+    private int wallCollisionCount;
+    [SerializeField] Vector2 myVel;
+
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "King") //jika terkena player
+        {
+            //Debug.Log("King Punch!");
+            //rb2d.AddForce(new Vector2(20,-15));
+            wallCollisionCount = 0;
+            DirectionChecker(coll.gameObject.transform);
+            if (isPositiveDirection) rb2d.AddForce(new Vector2(20,15));
+            else rb2d.AddForce(new Vector2(20,-15));
+        }
+        else if (coll.gameObject.name == "Pig") //jika terkena enemy
+        {
+            //Debug.Log("Pig Punch!");
+            //rb2d.AddForce(new Vector2(-20,-15));
+            wallCollisionCount = 0;
+            DirectionChecker(coll.gameObject.transform);
+            if (isPositiveDirection) rb2d.AddForce(new Vector2(-20,15));
+            else rb2d.AddForce(new Vector2(-20,-15));
+        }
+        else //jika terkena wall
+        {
+            wallCollisionCount = wallCollisionCount + 1;
+            //Debug.Log("Wall Collision! = " + wallCollisionCount);
+            if (wallCollisionCount > 6) GoBall();
+        }
+    }
+    
+    
+    #region PlayerDirectionChecker
+        Vector3 previousPosition;
+        [SerializeField]bool isPositiveDirection;
+        void DirectionChecker(Transform transform)
+        {
+        Vector2 currentPosition = transform.transform.position;
+        
+            if (currentPosition.y > previousPosition.y)
+            {
+                isPositiveDirection = true;
+            }
+            else if (currentPosition.y < previousPosition.y)
+            {
+                isPositiveDirection = false;
+            }
+            previousPosition = currentPosition;
+        }
+    #endregion
+
+
+    void Update()
+    {
+        
+    }
+
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,10 +9,13 @@ public class GameManager : MonoBehaviour
 
     public int scoreKing;
     public int scorePig;
+    public int winScore=100;
     public TMP_Text uIScoreKing;
     public TMP_Text uIScorePig;
     public TMP_Text uIWinTextCont;
     public GameObject uIWinLose;
+
+    public Animator camAnimator;
 
     public static GameManager instance;
     public SceneManagement sceneManagement;
@@ -30,12 +33,14 @@ public class GameManager : MonoBehaviour
             scorePig += 10;
             uIScorePig.text = scorePig.ToString();
             ScoreCheck();
+            camAnimator.SetTrigger("goShake");
         } 
         else 
         {
             scoreKing += 10;
             uIScoreKing.text = scoreKing.ToString();
             ScoreCheck();
+            camAnimator.SetTrigger("goShake");
         }
     }
 
@@ -45,13 +50,13 @@ public class GameManager : MonoBehaviour
     }
     private void ScoreCheck()
     {
-        if (scoreKing == 20) 
+        if (scoreKing == winScore) 
         {
             uIWinLose.SetActive(true);
             uIWinTextCont.text = "King Win!";
             Invoke("Delay",2.0f);
         }
-        else if (scorePig == 20)
+        else if (scorePig == winScore)
         {
             uIWinLose.SetActive(true);
             uIWinTextCont.text = "Pig Win!";
